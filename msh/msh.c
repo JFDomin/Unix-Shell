19f77d00b6a44899fd9a911c45e66f028e153a0d
JFDomin <150066117+JFDomin@users.noreply.github.com>
2024-04-12 03:54:20 +0000

    added copyright

--- msh/msh.c
+++ msh/msh.c
@@ -1,6 +1,7 @@
 // The MIT License (MIT)
 // 
 // Copyright (c) 2024 Trevor Bakker 
+// Copyright (c) 2024 Joshua Dominguez
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
